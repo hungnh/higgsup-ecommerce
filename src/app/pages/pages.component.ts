b2831758eee77622e77ed61b525b93838d7e394d
Hung Nguyen <hungnh.uet@gmail.com>
2019-01-31 15:20:34 +0700

    remove redundant files

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -5,10 +5,10 @@ import { MENU_ITEMS } from './pages-menu';
 @Component({
   selector: 'ngx-pages',
   template: `
-    <ngx-sample-layout>
+    <app-layout>
       <nb-menu [items]="menu"></nb-menu>
       <router-outlet></router-outlet>
-    </ngx-sample-layout>
+    </app-layout>
   `,
 })
 export class PagesComponent {
