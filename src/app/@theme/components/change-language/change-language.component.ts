f2a41a66bd0ab585a93eb631577a7ca0e8085abd
ScarDL <lengocnghiemduong@gmail.com>
2019-01-31 15:32:17 +0700

    i18n research - change name

--- src/app/@theme/components/change-language/change-language.component.ts
+++ src/app/@theme/components/change-language/change-language.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {TranslateService} from "@ngx-translate/core";
 
 @Component({
-  selector: 'change-language',
+  selector: 'ngx-change-language',
   templateUrl: './change-language.component.html',
   styleUrls: ['./change-language.component.scss']
 })
