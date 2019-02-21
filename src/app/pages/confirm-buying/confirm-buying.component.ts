7f47870d6b4175d3fe0533b7b11ed01c3b5e182c
ScarDL <lengocnghiemduong@gmail.com>
2019-02-21 15:35:13 +0700

    Set drop-down list for input

--- src/app/pages/confirm-buying/confirm-buying.component.ts
+++ src/app/pages/confirm-buying/confirm-buying.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {DeliveryInformation} from "../../@core/model/delivery-information";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'confirm-buying',
@@ -9,9 +10,20 @@ import {DeliveryInformation} from "../../@core/model/delivery-information";
 export class ConfirmBuyingComponent implements OnInit {
   deliveryInfo: DeliveryInformation = new DeliveryInformation();
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit() {
+  }
+
+  cancelBuying() {
+    this.router.navigate(['pages/cart']);
+  }
+
+  gotoPayment() {
+    this.router.navigate(['pages/payment']);
+  }
+
+  getAddress() {
   }
 }
