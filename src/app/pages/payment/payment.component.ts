d464d2c28bb16feb8c158e28707f84d07ebc9743
ScarDL <lengocnghiemduong@gmail.com>
2019-02-22 14:48:18 +0700

    Change some variables

--- src/app/pages/payment/payment.component.ts
+++ src/app/pages/payment/payment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'payment',
@@ -6,8 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment.component.scss']
 })
 export class PaymentComponent implements OnInit {
+  paymentWay: string = 'payCash';
+  bankLogos: Array<any> = [];
 
-  constructor() { }
+  constructor(public element: ElementRef) {
+  }
+
+  addImg() {
+    for (let i = 0; i <= 28; i++) {
+      this.bankLogos.push(i + '.jpg');
+    }
+  }
 
   ngOnInit() {
     this.addImg();
