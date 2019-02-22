22e207798db51c1604b5ab962df62c2e443be320
ScarDL <lengocnghiemduong@gmail.com>
2019-02-22 14:45:09 +0700

    Change color of some tag

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'ngx-footer',
@@ -9,11 +8,7 @@ import {TranslateService} from "@ngx-translate/core";
 })
 export class FooterComponent {
 
-  constructor(private translate: TranslateService) {
-    translate.get('footer.customer_support').subscribe((res: string) => {
-      console.log(res);
-      //=> 'hello world'
-    });
+  constructor() {
   }
 
 }
--- src/app/pages/payment/payment.component.ts
+++ src/app/pages/payment/payment.component.ts
@@ -10,6 +10,7 @@ export class PaymentComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.addImg();
   }
 
 }
