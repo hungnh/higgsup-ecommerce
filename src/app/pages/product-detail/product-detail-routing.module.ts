8f6c4cf11ee2aa5a303e7975bcaa2395c5be2375
ledevthang <leducthangdev2912@gmail.com>
2019-02-27 17:07:02 +0700

    commit image list
    commit image list

--- src/app/pages/product-detail/product-detail-routing.module.ts
+++ src/app/pages/product-detail/product-detail-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from "@angular/router";
-import {ProductDetailComponent} from "./product-detail.component";
+import {RouterModule, Routes} from '@angular/router';
+import {ProductDetailComponent} from './product-detail.component';
 
 
 const routes: Routes = [{
