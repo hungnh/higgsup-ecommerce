584a0618400fc6cac967a3bd5fda4231103eb141
ledevthang <leducthangdev2912@gmail.com>
2019-03-01 16:17:42 +0700

    done product details

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,7 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {Currency} from "../../@theme/glossary/currency.constant";
 import {ResponseDTO} from "../../@core/model/response-dto.model";
-import {Product} from "../../@core/model/product.model";
+import {Feedback, Product, RelatedProduct} from "../../@core/model/product.model";
+import {ProductDetailsService} from "../../@core/services/product-details.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {RatingCount} from "../../@core/model/rating-count.model";
 
 @Component({
   selector: 'product-detail',
@@ -18,8 +21,13 @@ export class ProductDetailComponent implements OnInit {
   starArray = [5, 4, 3, 2, 1];
   currency: string = '';
   product: Product = new Product();
+  feedbackList: Feedback = new Feedback();
+  relatedItemList: RelatedProduct = new RelatedProduct();
   id: number = 96123;
   queryParams: any;
+  listRating: Array<RatingCount>;
+  sumRating: number = 0;
+  imageList = [];
   constructor(private productService: ProductDetailsService, private router: Router, private activeRoute: ActivatedRoute) {
     this.queryParams = this.activeRoute.snapshot.queryParams;
   }
@@ -31,11 +39,19 @@ export class ProductDetailComponent implements OnInit {
     this.productService.getProductDetail(this.id).subscribe((res: ResponseDTO)=>{
       if(!res.responseMessage.messageCode) {
         this.product = res.responseMessage.data;
+        this.listRating = this.product.ratingCount;
+        this.listRating.forEach(feedback => {
+          this.sumRating += feedback.counting;
+        });
+        this.imageList = this.product.imgUrl.split(";", 10);
+        this.imageList.pop();
         this.newPrice = this.product.unitPrice * (100 - this.product.discountPercent)/100;
       } else {
         this.router.navigate(['pages/not-found']);
       }
     });
+    this.getFeedBack(this.id);
+    this.getRelatedProduct(this.id);
   }
 
 
@@ -56,5 +72,24 @@ export class ProductDetailComponent implements OnInit {
     // console.log(event);
     // this.imagePreview = this.imageItem;
   }
-
+  getFeedBack(id) {
+    this.productService.getFeedback(id).subscribe((res: ResponseDTO) => {
+      if (!res.responseMessage.messageCode) {
+        this.feedbackList = res.responseMessage.data;
+        console.log(this.feedbackList);
+      } else {
+        this.router.navigate(['pages/not-found']);
+      }
+    })
+  }
+  getRelatedProduct(id) {
+    this.productService.getRelatedProduct(id).subscribe(res => {
+      if (!res.messageCode) {
+        this.relatedItemList = res.data;
+        console.log(this.relatedItemList);
+      } else {
+        this.router.navigate(['pages/not-found']);
+      }
+    })
+  }
 }
