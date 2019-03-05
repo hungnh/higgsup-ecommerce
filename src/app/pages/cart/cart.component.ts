3b19313e4ef77c66038ac23c58282336963fd4f4
ScarDL <lengocnghiemduong@gmail.com>
2019-03-05 17:21:47 +0700

    Edit a little UI payment flow

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -17,8 +17,8 @@ import {DataTransferService} from "../../@core/services/data-transfer.service";
 })
 export class CartComponent implements OnInit {
 
-  productList: Array<ProductModel> = [];
-  count: number = null;
+  productList: Array<ProductModel>;
+  count: number;
   subTotal: number = null;
   currency: string = '';
   allowDelete: boolean = false;
@@ -33,11 +33,6 @@ export class CartComponent implements OnInit {
   ngOnInit() {
     this.currency = Currency.USD;
     this.getCart();
-    if(!this.productList) {
-      this.count = this.productList.length;
-    } else {
-      this.count = 0;
-    }
     this.dataService.currentEvent.subscribe(data => {
       if (data) {
         this.cartService.removeFromCart(data.dataTransfer).subscribe((res: ResponseDTO) => {
@@ -61,6 +56,11 @@ export class CartComponent implements OnInit {
         this.productList.forEach(product => {
           this.subTotal += product.amount * (100 - product.discountPercent) / 100 * product.unitPrice;
         });
+        if (!this.productList) {
+          this.count = 0;
+        } else {
+          this.count = this.productList.length;
+        }
       }
     })
   }
